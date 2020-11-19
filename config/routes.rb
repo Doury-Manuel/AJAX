e9cbd6b92ceaa2b1e9f9f60f211d400947869b3d
Giaforax <doury.manuel@gmail.com>
2020-11-19 16:21:23 +0100

    ajax_1_working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :emails
+
+
   devise_for :users
-  root to: "home#index"
+  root to: "email#index"
   resources :tasks, except: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-3.times do
-  my_category = Category.create(title: Faker::Book.genre)
-  3.times do
-    my_task = Task.new(title: Faker::Book.title,
-                       deadline: Faker::Date.forward(23),
-                       image: Faker::Avatar.image)
-    my_task.category = my_category
-    my_task.save
+  20.times do
+    Email.create(object: Faker::Quote.famous_last_words)                  
   end
-end
 puts "seeding db"
